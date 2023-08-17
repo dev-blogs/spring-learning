c54068e7c530c71daf637a8d8890dcdfe39ffa7f
zheka <zheka0783@yandex.ru>
2023-08-17 21:42:59 +0300

    Code refactoring. Removed unused imports from App class

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,9 +1,6 @@
 package com.example;
 
 import org.springframework.beans.factory.BeanFactory;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 import com.example.beans.MessageRenderer;
 import com.example.config.Factory;
 
