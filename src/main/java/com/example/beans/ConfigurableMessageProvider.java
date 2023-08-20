104f9b11c7fc7612eda0e1e7388bc79dea0e6b87
zheka <zheka0783@yandex.ru>
2023-08-20 14:21:30 +0300

    4-2 Code development. Changed MessageProvider implementation
    + Added logic which takes configurable parameter for MessageProvider

---
+++ src/main/java/com/example/beans/ConfigurableMessageProvider.java
@@ -0,0 +1,14 @@
+package com.example.beans;
+
+public class ConfigurableMessageProvider implements MessageProvider {
+	private final String message;
+	
+	public ConfigurableMessageProvider(String message) {
+		this.message = message;
+	}
+	
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
--- src/main/java/com/example/beans/HelloWorldMessageProvider.java
+++
@@ -1,8 +0,0 @@
-package com.example.beans;
-
-public class HelloWorldMessageProvider implements MessageProvider {
-    @Override
-    public String getMessage() {
-        return "test";
-    }
-}
