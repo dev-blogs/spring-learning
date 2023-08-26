7d3c06b1699131a11af4662a51e577036e3fbfa6
dev-blogs <gcp.learn00007@gmail.com>
2023-08-26 19:02:25 +0300

    master. Code refactor. Renamed variable

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -6,8 +6,8 @@ import com.example.config.Factory;
 
 public class App {
 	public static void main(String[] args) {
-		BeanFactory factory = Factory.getBeanFactory("src/main/resources/context.xml");
-		MessageRenderer mr = (MessageRenderer) factory.getBean("renderer");
+		BeanFactory context = Factory.getBeanFactory("src/main/resources/context.xml");
+		MessageRenderer mr = (MessageRenderer) context.getBean("renderer");
 		mr.render();
 	}
 }
