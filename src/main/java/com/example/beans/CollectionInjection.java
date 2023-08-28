de3d76e2b39863e543c40270df099a451e931897
dev-blogs <gcp.learn00007@gmail.com>
2023-08-28 20:20:09 +0300

    4-5 code refactoring
    + Refactored CollectionInjection class

--- src/main/java/com/example/beans/CollectionInjection.java
+++ src/main/java/com/example/beans/CollectionInjection.java
@@ -1,14 +1,15 @@
 package com.example.beans;
 
-import org.springframework.stereotype.Service;
-import javax.annotation.Resource;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
+import org.springframework.stereotype.Service;
+import javax.annotation.Resource;
 
 /**
- * See <a href="https://stackoverflow.com/questions/22589302/where-is-javax-annotation">here</a> for understanding @Resource annotation
+ * See <a href="https://stackoverflow.com/questions/22589302/where-is-javax-annotation">here</a>
+ * for understanding @Resource annotation
  */
 @Service("injectCollection")
 public class CollectionInjection {
